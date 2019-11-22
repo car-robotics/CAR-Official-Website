f6b99f6e2b58d56a158d83937ab2ce26733051b5
Matt Wells <mwells21@uncc.edu>
2019-11-21 20:03:04 -0500

    Sponsors Page Revamp (#1)
    * Sponsors page updates and background switch due to scrollbar
    
    * Simplifying sponsors page
    
    * Slight adjustments
    
    * Sponsor cards look good!
    
    * Finish sponsors page

---
+++ src/Sponsors/SponsorCard.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link, withStyles, Card } from "@material-ui/core";
+import EasyToSeeTooltip from "../Main/EasyToSeeTooltip";
+import Image from "material-ui-image";
+import { ListItem } from "../Sponsors/LevelCard";
+import "./Sponsors.scss";
+
+interface SponsorsCardProps{
+    orgName: string;
+    orgLink?: string;
+    img: string;
+}
+
+const OrgCard = withStyles({
+    root: {
+        display: "flex",
+        fontFamily: "Inconsolata",
+        textAlign: "center",
+        backgroundColor: "inherit",
+        borderRadius: "1rem",
+        alignItems: "Center",
+        margin: "auto",
+    },
+})(Card);
+
+export default function SponsorCard(props: SponsorsCardProps) {
+    const {orgName, orgLink, img} = props;
+    return (
+        <>
+            <OrgCard>
+                {orgLink && <EasyToSeeTooltip title={`${orgName} - ${orgLink}`}>
+                    <Link href={orgLink} target="_blank">
+                        <div className="container">
+                            <Image
+                                src={img}
+                                imageStyle={{
+                                    display: "block",
+                                    width: "13rem",
+                                    height: "15rem",
+                                    transition: ".5s ease",
+                                    backfaceVisibility: "hidden",
+                                    borderStyle: "solid",
+                                    borderRadius: "10px",
+                                    borderWidth: "12px",
+                                    borderColor: "#212b31",
+                                }}
+                                style={{
+                                    width: "14.5rem",
+                                    height: "16.5rem",
+                                    paddingTop: 0,
+                                }}
+                            />
+                            <div className="overlay backgroundBlack">
+                                <ListItem align="center" className="middleText" style={{fontSize: "1.5rem"}}>
+                                    Learn More
+                                </ListItem>
+                            </div>
+                        </div>
+                    </Link>
+                </EasyToSeeTooltip>}
+
+                {!orgLink && <div className="container">
+                    <Image
+                        src={img}
+                        imageStyle={{
+                            display: "block",
+                            width: "13rem",
+                            height: "15rem",
+                            transition: ".5s ease",
+                            backfaceVisibility: "hidden",
+                            borderStyle: "solid",
+                            borderRadius: "10px",
+                            borderWidth: "12px",
+                            borderColor: "#212b31",
+                        }}
+                        style={{
+                            width: "14.5rem",
+                            height: "16.5rem",
+                            paddingTop: 0,
+                        }}
+                    />
+                    <div className="overlay backgroundBlack">
+                        <ListItem align="center" className="middleText" style={{fontSize: "1.5rem"}}>
+                            {orgName}
+                        </ListItem>
+                    </div>
+                </div>}
+            </OrgCard>
+        </>
+    );
+}    
--- src/Sponsors/Sponsors.tsx
+++ src/Sponsors/Sponsors.tsx
@@ -1,61 +1,31 @@
 import React from "react";
-import { Typography, Card, Paper, makeStyles, withStyles, createStyles, Theme, IconButton } from "@material-ui/core";
-import { Email } from "@material-ui/icons";
-import { useCurrentWidth } from "react-socks";
-import support from "../Images/roboSuport.png";
-import bannerImg from "../Images/bannerCHA.jpg";
+import { Typography, Paper, makeStyles, createStyles, Theme, Divider, Link } from "@material-ui/core";
 import { DocumentTitle } from "../Main/DocumentTitle";
-import "./Sponsors.scss";
-import Walmart from "./Walmart";
-import SolidWork from "./SolidWork";
-import Advanced from "./Advanced";
-import Ctc from "./CTC";
-import JoinTeam from "./JoinTeam";
-
-
-const SponsorCard = withStyles({
-    root: {
-        display: "flex",
-        fontFamily: "Inconsolata",
-        textAlign: "center",
-        backgroundColor: "inherit",
-        borderRadius: "1rem",
-        alignItems: "Center",
-        boxShadow: "inset 0px 0px 9px 1px",
-    },
-})(Card);
+import SponsorCard from "./SponsorCard";
+import LevelCard from "./LevelCard";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        //Format sponsorType card
-        tyCard: {
-            width: "85%",
-            display: "initial",
-            gridArea: "tyTxt",
-            padding: "10px",
+        container: {
+            padding: "2rem",
             margin: "auto",
             backgroundColor: "#212b31",
             color: "#B3A369",
+            borderRadius: "1rem",
         },
-        sectionCard: {
-            display: "flex",
-            color: "#212b31",
-            boxShadow: "inset 0px 0px 9px 2px",
-            opacity: .97,
-            padding: "10px",
-            margin: "auto auto auto 10rem",
+        levelSection: {
+            margin: "3rem 0 0 3rem",
+            display: "inline-flex",
+            width: "95%",
+            borderRadius: "1rem",
         },
-        SideCard: {
-            display: "flex",
-            color: "#212b31",
-            backgroundColor: "#B3A369",
-            boxShadow: "inset 0px 0px 23px 10px",
-            padding: "10px",
-            height: "100%",
-            width: "auto",
-        },
-        divider: {
-            margin: "10px",
+        sponsorCards: {
+            display: "inline-flex",
+            width: "76%",
+            justifyContent: "space-evenly",
+            padding: "1rem",
+            boxShadow: "inset 0px 0px 9px 2px black",
+            borderRadius: "0px 1rem 1rem 0px",
         },
     }),
 );
@@ -63,114 +33,85 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function Sponsors() {
     //Set the default sizing only used for (mobile version)
-    const width = useCurrentWidth();
+    // const width = useCurrentWidth();
     const classes = useStyles();
-    const max_width = width > 1000 ? "19rem" : "";
-    const card_margin = width > 1000 ? "" : "10px";
 
-    //Used to set website tab name
     DocumentTitle({ title: "Sponsors" });
 
     return (
-        //Create a div for page container
-        <div className={width > 1000 ? "sponsors-container" : "sponsors-container-mobile"}>
+        <div style={{margin: "5rem auto", width: "95%"}}>
 
-            {/* Banner Image */}
-            <img src={bannerImg} className="banner" alt="Banner" />
+            <Paper className={classes.container} >
+                <Typography align="center" variant="h2" >
+                    Thank You To Our Sponsors For Their Support!
+                </Typography>
 
-            {/* Thank You Text */}
-            <Paper className={classes.tyCard} elevation={24} >
-                <Typography align="center" className="ty-text" variant="h4">
-                    - Thank You To Our Sponsors For Their Support! -</Typography>
+                <Divider style={{ backgroundColor: "#B3A369", marginTop: "10px" }} variant="middle" />
+
+            <div className={classes.levelSection} >
+                <LevelCard
+                    level="Gold"
+                />
+
+                <div className={classes.sponsorCards} style={{backgroundColor: "#ead286"}} >
+                    <SponsorCard
+                        img={"https://img.pngio.com/walmart-logo-free-transparent-png-logos-walmart-logo-png-960_899.jpg"}
+                        orgName={"Walmart"}
+                        orgLink={"https://walmart.com/"}
+                    />
+                    <SponsorCard
+                        img={"http://imatrixsol.com/img/solidworks.png"}
+                        orgName={"SolidWorks"}
+                        orgLink={"https://www.solidworks.com/"}
+                    />
+                    <SponsorCard
+                        img={"https://retohercules.com/images/office-people-clipart-9.jpg"}
+                        orgName={"Your logo here!"}
+                    />
+                </div>
+            </div>
+
+            <div className={classes.levelSection}>
+                <LevelCard
+                    level="Silver"
+                />
+
+                <div className={classes.sponsorCards} style={{backgroundColor: "#c1c0c0"}} >
+                    <SponsorCard 
+                        img={"https://enventyspartners.com/wp-content/uploads/2017/09/cropped-en-favicon.gif"}
+                        orgName={"Envyntus Partners"}
+                        orgLink={"https://enventyspartners.com/"}
+                    />
+                    <SponsorCard
+                        img={"https://retohercules.com/images/office-people-clipart-9.jpg"}
+                        orgName={"Your logo here!"}
+                    />
+                </div>
+            </div>
+
+            <div className={classes.levelSection}>
+                <LevelCard
+                    level="Bronze"
+                />
+
+                <div className={classes.sponsorCards} style={{backgroundColor: "#bda780"}} >
+                    <SponsorCard
+                        img={"https://retohercules.com/images/office-people-clipart-9.jpg"}
+                        orgName={"Your logo here!"}
+                    />
+                </div>
+            </div>
+
+            
             </Paper>
 
-            {/* HandShake Image */}
-            {/*"https://www.pngkey.com/detail/u2q8i1w7q8w7e6w7_04-apr-robotic-process-automation-robot-and-human/*/}
-            <img src={support} className="handshakeImg" alt="HandShake" />
-
-            {/* leftSide Card */}
-            <SponsorCard className="leftCard" style={{ gridArea: "leftCard", backgroundColor: "#B3A369", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
+            <Paper className={classes.container} style={{marginTop: "2rem", width: "60%"}}>
                 <Typography align="center" variant="h5">
-                    Interested In Sponsoring?<br />
-                </Typography>
-            </SponsorCard>
-
-            {/* rightSide Card */}
-            <SponsorCard className="rightCard" style={{ gridArea: "rightCard", backgroundColor: "#B3A369", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                <Typography align="center" variant="h5">
-                    Please Contact Sam Luu
-                    <IconButton href="mailto:car-robotics@uncc.edu/" target="_blank">
-                        <Email fontSize="large" htmlColor="#212b31" />
-                    </IconButton>
-                </Typography>
-            </SponsorCard>
-
-
-            {/* GOLD Sponsors Section */}
-            <Paper className={classes.sectionCard} style={{ gridArea: "sec1", backgroundColor: "#B3A369", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                {/* GOLD Sponsors Description */}
-                <Typography align="center" className="desc cBlack" style={{ marginLeft: 20, marginRight: 20 }}>
-                    <Typography component="p" className="secTitle" variant="h4">Gold Level:<br />($250+)</Typography>
-                    <Typography component="p" variant="h5" className="leftAlign">Includes:</Typography>
-                    <Typography component="ul">
-                        <Typography style={{ fontFamily: "Inconsolata" }} component="li">Your Companys Logo On Clubs Shirts</Typography>
-                        <Typography style={{ fontFamily: "Inconsolata" }} component="li">Logo On Club Merchendise</Typography>
-                        <Typography style={{ fontFamily: "Inconsolata" }} component="li">Same Benefits As The Silver &amp; Green Level Sponsorship</Typography>
-                    </Typography>
+                    To become a sponsor contact Sam Luu, our Outreach Officer, at <br/><Link href="mailto:car-robotics@uncc.edu" style={{color: "#00713d"}}>car-robotics@uncc.edu</Link>
                 </Typography>
             </Paper>
 
-            {/* GOLD Sponsors Cards */}
-            <SponsorCard className="gSpon" style={{ gridArea: "gSpon1", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                <Walmart classes={classes} />
-            </SponsorCard>
-            <SponsorCard className="gSpon" style={{ gridArea: "gSpon2", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                <SolidWork classes={classes} />
-            </SponsorCard>
-            <SponsorCard className="gSpon" style={{ gridArea: "gSpon3", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                <JoinTeam classes={classes} />
-            </SponsorCard>
-
-
-            {/*SILVER Sponsors Section */}
-            <Paper className={classes.sectionCard} style={{ gridArea: "sec2", backgroundColor: "#A9A9A9", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                {/* Silver Sponsors Description */}
-                <Typography align="center" className="desc cBlack" style={{ marginLeft: 20, marginRight: 20 }}>
-                    <Typography component="p" className="secTitle" variant="h4">Silver Level:<br />($100+)</Typography>
-                    <Typography component="p" variant="h5" className="leftAlign">Includes:</Typography>
-                    <Typography component="ul">
-                        <Typography style={{ fontFamily: "Inconsolata" }} component="li">Logo On All Of Our Robots</Typography>
-                        <Typography style={{ fontFamily: "Inconsolata" }} component="li">A Shout Out Before Any Robot Public Demonstrations</Typography>
-                        <Typography style={{ fontFamily: "Inconsolata" }} component="li">Same Benefits As The Green Level Sponsorship</Typography>
-                    </Typography>
-                </Typography>
-            </Paper>
-            {/* Sponsors Cards */}
-            <SponsorCard className="gSpon" style={{ gridArea: "sSpon1", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                <Advanced classes={classes} />
-            </SponsorCard>
-            <SponsorCard className="gSpon" style={{ gridArea: "sSpon2", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                <Ctc classes={classes} />
-            </SponsorCard>
-
-            {/* GREEN Sponsors Section */}
-            <Paper className={classes.sectionCard} style={{ gridArea: "sec3", backgroundColor: "#2d855f", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                {/* Green Sponsors Description */}
-                <Typography align="center" className="desc cBlack" variant="h5" style={{ marginLeft: 20, marginRight: 20 }}>
-                    <Typography component="p" className="secTitle" variant="h4">Green Level:<br />($-100)</Typography>
-                    <Typography component="p" variant="h5" className="leftAlign">Includes:</Typography>
-                    <Typography component="ul">
-                        <Typography style={{ fontFamily: "Inconsolata" }} component="li">Logo &amp; Link To Your Company's Website Will Be Displayed On CAR'S Sponsors Page</Typography>
-                    </Typography>
-                </Typography>
-            </Paper>
-            {/* Sponsors Cards */}
-
-            <SponsorCard className="gSpon" style={{ gridArea: "grSpon1", maxWidth: max_width, marginLeft: card_margin, marginRight: card_margin }}>
-                <JoinTeam classes={classes} />
-            </SponsorCard>
-
-        </div>
+        </div >
     );
 }
 
