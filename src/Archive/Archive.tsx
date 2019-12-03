e5f8d33b4f7ed09f9fc14838add6fa0c2399b5b2
mattwells19 <mwells21@uncc.edu>
2019-12-03 00:24:00 -0500

    Added menu for archive content

--- src/Archive/Archive.tsx
+++ src/Archive/Archive.tsx
@@ -1,21 +1,47 @@
 import React, { Component } from "react";
 import './Archive.scss';
-import AdvancedGridList from './ImageGrid';
-import { Typography, Link } from "@material-ui/core";
+import AdvancedGridList, { ArchiveCategory } from './ImageGrid';
+import { Typography, Link, MenuList, MenuItem, withStyles } from "@material-ui/core";
 import ReactPlayer from "react-player";
 import PageFade from "../Utils/PageFade";
 import { DocumentTitle } from "../Utils/DocumentTitle";
+import { ContentBackground } from "../Utils/ContentBackground";
+import GoldDivider from "../Utils/GoldDivider";
 
+const CollectionItem = withStyles({
+    root: {
+        fontSize: "1.5rem",
+    },
+})(MenuItem);
 
-class Archive extends Component {
+interface ArchiveState { selectedIndex: number }
+
+class Archive extends Component<{}, ArchiveState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            selectedIndex: 0,
+        };
+    }
+
     render() {
         DocumentTitle({ title: "Archive" });
+
+        const collectionItems = [
+            "All",
+            "Outreach/Events",
+            "Sprint Reviews",
+            "The Robot",
+        ]
+
         return (
             <PageFade>
                 <div className="archivePageContent">
-                    <div id="msgs">
+                    {/* <div id="msgs">
                         <Typography variant='h1' id="archiveWelcome"> Welcome to The Archive</Typography>
-                        <Typography variant='h3' id="welcomeMsg">Check out our video compilation and photo gallery of previous competitions and club activities</Typography>
+                        <Typography variant='h3' id="welcomeMsg">
+                            Check out our video compilation and photo gallery of previous competitions and club activities
+                        </Typography>
                         <div>
                             <ReactPlayer
                                 url="https://www.youtube.com/embed/QIC3dg53WWg"
@@ -26,11 +52,27 @@ class Archive extends Component {
                         </div>
                         <Typography variant='h3' id="checkoutMore">Watch more videos on <Link style={{ color: "#046A38" }} href="https://www.youtube.com/results?search_query=Charlotte+Area+Robotics+" target="_blank">YouTube </Link>
                         </Typography>
-                    </div>
-                    <div id="photoSection">
-                        <Typography variant='h3' id="photoGal"> - Photo Gallery - </Typography>
-                        <AdvancedGridList />
-                    </div>
+                    </div> */}
+                    <ContentBackground className="menu-container">
+                        <MenuList>
+                            {collectionItems.map((option, index) => {
+                                return (
+                                    <CollectionItem
+                                        selected={index === this.state.selectedIndex}
+                                        className="archive-selection"
+                                        onClick={() => this.setState({ selectedIndex: index })}
+                                    >
+                                        {option}
+                                    </CollectionItem>
+                                )
+                            })}
+                        </MenuList>
+                    </ContentBackground>
+                    <ContentBackground>
+                        <Typography variant='h3' className="collage-header">Photo Gallery</Typography>
+                        <GoldDivider />
+                        <AdvancedGridList section={this.state.selectedIndex} />
+                    </ContentBackground>
                 </div>
             </PageFade>
         );
--- src/Archive/ImageGrid.tsx
+++ src/Archive/ImageGrid.tsx
@@ -1,11 +1,23 @@
 
 import React from 'react';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import { GridList, GridListTile, GridListTileBar } from "@material-ui/core";
+import Image from "material-ui-image";
 import './Archive.scss';
 
+export enum ArchiveCategory {
+  all,
+  outreach,
+  sprintReview,
+  robot,
+}
+
+interface Tile {
+  img: string;
+  title: string;
+  category: ArchiveCategory;
+  featured: boolean;
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -25,139 +37,165 @@ const useStyles = makeStyles((theme: Theme) =>
       background:
         'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
         'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-      },
+    },
     icon: {
       color: '#bfbfbf',
     },
   }),
 );
 
-const tileData = [
+const tileData: Tile[] = [
   {
     img: '/ReturningMembers.jpg',
     title: 'Returning Members',
+    category: ArchiveCategory.all,
     featured: true,
   },
   {
     img: '/Robot1.jpg',
     title: 'Robot',
+    category: ArchiveCategory.robot,
     featured: false,
   },
   {
     img: '/Robot2.jpg',
     title: 'Robot',
+    category: ArchiveCategory.robot,
     featured: false,
   },
   {
     img: '/Outreach/ArduinoWksp-6.jpg',
     title: 'Arduino WorkSpace',
+    category: ArchiveCategory.outreach,
     featured: true,
   },
   {
     img: '/Outreach/ArduinoWksp-5.jpg',
     title: 'Arduino WorkSpace',
+    category: ArchiveCategory.outreach,
     featured: false,
-  },  
+  },
   {
-      img: '/Outreach/ArduinoWksp-2.jpg',
-      title: 'Arduino WorkSpace',
-      featured: false,
+    img: '/Outreach/ArduinoWksp-2.jpg',
+    title: 'Arduino WorkSpace',
+    category: ArchiveCategory.outreach,
+    featured: false,
   },
   {
     img: '/Outreach/ArduinoWksp-8.jpg',
     title: 'Arduino WorkSpace',
+    category: ArchiveCategory.outreach,
     featured: true,
   },
   {
     img: '/Outreach/BobsBash1-2.jpg',
     title: 'Bobs Bash',
+    category: ArchiveCategory.outreach,
     featured: true,
   },
   {
     img: '/Outreach/BobsBash1-1.jpg',
     title: 'Bobs Bash',
+    category: ArchiveCategory.outreach,
     featured: false,
   },
   {
     img: '/Outreach/DiscoveryPlace2.jpg',
     title: 'Discovery Place',
+    category: ArchiveCategory.outreach,
     featured: false,
   },
   {
     img: '/Outreach/DiscoveryPlace1.jpg',
     title: 'Discovery Place',
+    category: ArchiveCategory.outreach,
     featured: true,
   },
   {
     img: '/Outreach/Elementary2.jpg',
     title: 'Elementary',
+    category: ArchiveCategory.outreach,
     featured: true,
   },
   {
     img: '/Outreach/Elementary1.jpg',
     title: 'Elementary',
+    category: ArchiveCategory.outreach,
     featured: false,
   },
   {
     img: '/Outreach/NCScience2.jpg',
     title: 'NCScience',
+    category: ArchiveCategory.outreach,
     featured: false,
   },
   {
     img: '/Outreach/NCScience1.jpg',
     title: 'NCScience',
+    category: ArchiveCategory.outreach,
     featured: true,
   },
   {
     img: '/SprintReviews/EndofSprint2-1.jpg',
     title: 'Sprint Reviews',
+    category: ArchiveCategory.sprintReview,
     featured: true,
   },
   {
     img: '/SprintReviews/EndofSprint2-2.jpg',
     title: 'Sprint Reviews',
+    category: ArchiveCategory.sprintReview,
     featured: false,
   },
   {
     img: '/SprintReviews/EndofSprint2-3.jpg',
     title: 'Sprint Reviews',
+    category: ArchiveCategory.sprintReview,
     featured: false,
   },
   {
     img: '/SprintReviews/EndofSprint2-4.jpg',
     title: 'Sprint Reviews',
+    category: ArchiveCategory.sprintReview,
     featured: true,
   },
   {
     img: '/SprintReviews/EndofSprint2-5.jpg',
     title: 'Sprint Reviews',
+    category: ArchiveCategory.sprintReview,
     featured: false,
   },
   {
     img: '/SprintReviews/EndofSprint2-7.jpg',
     title: 'Sprint Reviews',
+    category: ArchiveCategory.sprintReview,
     featured: false,
   },
 ];
 
-export default function AdvancedGridList() {
+interface ImageGridProps {
+  section: ArchiveCategory;
+}
+
+export default function AdvancedGridList(props: ImageGridProps) {
   const classes = useStyles();
-  
-    return (
-      <div className={classes.root}>
+
+  return (
+    <div className={classes.root}>
       <GridList cellHeight={340} spacing={1} className={classes.gridList}>
-          {tileData.map(tile => (
+        {tileData.map(tile => (
+          (tile.category === props.section || props.section === ArchiveCategory.all) &&
           <GridListTile key={tile.img} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
-            <img src={tile.img} alt={tile.title} />
-          <GridListTileBar
-            title={tile.title}
-            titlePosition="top"
-            actionPosition="left"
-            className={classes.titleBar}
-          />
+            <img src={tile.img} />
+            <GridListTileBar
+              title={tile.title}
+              titlePosition="top"
+              actionPosition="left"
+              className={classes.titleBar}
+            />
           </GridListTile>
-          ))}
+        ))}
       </GridList>
-      </div>
-    );
-  }
+    </div>
+  );
+}
