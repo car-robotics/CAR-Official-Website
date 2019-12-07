0877874aa4fb42a76c39427a7a93873cdf853098
mattwells19 <mwells21@uncc.edu>
2019-12-07 14:59:46 -0500

    Competition mobilized and Iframe replaced with iframe

--- src/Competition/Competition.tsx
+++ src/Competition/Competition.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import CountdownTimer from "./CountdownTimer";
 import Image from "material-ui-image";
 import { Typography } from "@material-ui/core";
@@ -6,48 +6,48 @@ import PageFade from "../Utils/PageFade";
 import { DocumentTitle } from "../Utils/DocumentTitle";
 import GoldDivider from "../Utils/GoldDivider";
 import { ContentBackground } from "../Utils/ContentBackground";
-import Iframe from "react-iframe";
 import "./Competition.scss";
 import GreenLink from "../Utils/GreenLink";
+import { useCurrentWidth } from "react-socks";
 
-class Competition extends Component {
-    render() {
-        DocumentTitle({ title: "Competition" });
-        return (
-            <PageFade>
-                <div style={{ marginTop: "5rem" }}>
-                    <CountdownTimer />
-                    <ContentBackground elevation={24} className="competition-content">
-                        <Image
-                            src={"https://www.ieeeuncc.com/uploads/1/2/4/2/124254028/ieeefulllogo_orig.png"}
-                            style={{ backgroundColor: "transparent", paddingTop: "" }}
-                            imageStyle={{ height: "", width: "100%", position: "" }}
-                        />
-                        <Typography align="center" variant="h4" style={{ lineHeight: "3.5rem" }}>
-                            The <GreenLink text="Institute of Electrical and Electronics Engineers (IEEE)" link="https://www.ieee.org/" />{" "}
-                            Region 3 annually hosts student competitions as part of the Southeast Conference.
-                            Of these competitions, the hardware competition involves students from different
-                            engineering fields to work together to build and compete with fully autonomous
-                            robots. This year the competition will be hosted in Raleigh North Carolina on March 14th
+function Competition() {
+    DocumentTitle({ title: "Competition" });
+
+    const mobile = useCurrentWidth() < 1000;
+    return (
+        <PageFade>
+            <div style={{ marginTop: "5rem" }}>
+                <CountdownTimer />
+                <ContentBackground elevation={24} className="competition-content">
+                    <Image
+                        src={"https://www.ieeeuncc.com/uploads/1/2/4/2/124254028/ieeefulllogo_orig.png"}
+                        style={{ backgroundColor: "transparent", paddingTop: "" }}
+                        imageStyle={{ height: "", width: "100%", position: "" }}
+                    />
+                    <Typography align="center" variant={mobile ? "h6" : "h4"} style={{ lineHeight: mobile ? "2.5rem" : "3.5rem" }}>
+                        The <GreenLink text="Institute of Electrical and Electronics Engineers (IEEE)" link="https://www.ieee.org/" />{" "}
+                        Region 3 annually hosts student competitions as part of the Southeast Conference.
+                        Of these competitions, the hardware competition involves students from different
+                        engineering fields to work together to build and compete with fully autonomous
+                        robots. This year the competition will be hosted in Raleigh North Carolina on March 14th
                             (for <GreenLink link="https://attend.ieee.org/southeastcon-2020/student-program/student-hardware-competition/" text="Pi Day!" />).
                         </Typography>
-                    </ContentBackground>
-                    <ContentBackground elevation={24} className="competition-content">
-                        <Typography variant="h2" align="center">
-                            The Rules: Pi Day 2020
+                </ContentBackground>
+                <ContentBackground elevation={24} className="competition-content">
+                    <Typography variant={mobile ? "h4" : "h2"} align="center">
+                        The Rules:{mobile ? <br /> : " "}Pi Day 2020
                         </Typography>
-                        <GoldDivider />
-                        <Iframe
-                            title="rules"
-                            url="https://drive.google.com/file/d/1rc0wpz5IDh3fPyaSZwz_TqPAv9dnhEcB/preview"
-                            width="100%"
-                            height="900px"
-                        />
-                    </ContentBackground>
-                </div>
-            </PageFade>
-        );
-    }
+                    <GoldDivider />
+                    <iframe
+                        title="rules"
+                        src="https://drive.google.com/file/d/1rc0wpz5IDh3fPyaSZwz_TqPAv9dnhEcB/preview"
+                        width="100%"
+                        style={{ height: mobile ? "50vh" : "85vh" }}
+                    />
+                </ContentBackground>
+            </div>
+        </PageFade>
+    );
 }
 
 export default Competition;
--- src/Competition/TimerDisplay.tsx
+++ src/Competition/TimerDisplay.tsx
@@ -3,15 +3,19 @@ import { Typography } from "@material-ui/core";
 import GoldDivider from "../Utils/GoldDivider";
 import { ContentBackground } from "../Utils/ContentBackground";
 import "./Competition.scss";
+import { useCurrentWidth } from "react-socks";
 
 export default function TimerDisplay(props: any) {
+
+    const mobile = useCurrentWidth() < 1000;
+
     return (
         <ContentBackground elevation={24} className="countdown">
-            <Typography style={{ lineHeight: "3.5rem" }} variant="h2" align="center">
+            <Typography style={{ lineHeight: mobile ? "2.5rem" : "3.5rem" }} variant={mobile ? "h4" : "h2"} align="center">
                 Countdown to Competition!
             </Typography>
             <GoldDivider />
-            <Typography style={{ lineHeight: "3.5rem" }} variant="h3" align="center">
+            <Typography style={{ lineHeight: mobile ? "2.5rem" : "3.5rem" }} variant={mobile ? "h5" : "h3"} align="center">
                 {props.display}
             </Typography>
         </ContentBackground>
