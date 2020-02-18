cfbe151d5e687ad48341aa2050efed172dbcc1cc
Matt <mwells21@uncc.edu>
2020-02-18 16:02:50 -0500

    Images added

--- src/Archive/ImageGrid.tsx
+++ src/Archive/ImageGrid.tsx
@@ -1,27 +1,26 @@
-
-import React from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import { GridList, GridListTile, GridListTileBar } from "@material-ui/core";
 import Image from "material-ui-image";
-import { tileData, ArchiveCategory, Tile } from './ImageList';
-import './Archive.scss';
-import { MobileContext } from '../Context/MobileContext';
+import { tileData, ArchiveCategory, Tile } from "./ImageList";
+import "./Archive.scss";
+import { MobileContext } from "../Context/MobileContext";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      height: "94%",
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+      height: "94%"
     },
     titleBar: {
       background:
-        'linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.25))',
-      height: "fit-content",
-    },
-  }),
+        "linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.25))",
+      height: "fit-content"
+    }
+  })
 );
 
 interface ImageGridProps {
@@ -37,38 +36,49 @@ export default function ImageGrid(props: ImageGridProps) {
   return (
     <MobileContext.Consumer>
       {mobile => (
-        <div className={classes.root} onScroll={(e) => props.handleScroll(e)}>
-          <GridList id="image-collage" spacing={1} cellHeight="auto" className="gridList">
-            {tileData.map((tile, index) => (
-              (tile.category === props.section || props.section === ArchiveCategory.all) &&
-              <GridListTile
-                key={tile.img}
-                cols={index === 0 ? 2 : 1}
-                style={{ padding: "0.25rem" }}
-              >
-                <Image
-                  src={tile.img}
-                  disableError
-                  onClick={() => props.handleImageClick(tile)}
-                  style={{
-                    backgroundColor: "transparent",
-                    paddingTop:
-                      index === 0 && props.section === ArchiveCategory.all ? "40%" :
-                        tile.orientation === "horizontal" ? "75%" : "100%",
-                  }}
-                  imageStyle={{
-                    height: "",
-                  }}
-                />
-                <GridListTileBar
-                  title={tile.title}
-                  titlePosition="top"
-                  actionPosition="left"
-                  className={classes.titleBar}
-                  style={{ padding: mobile ? "0" : "0.5rem" }}
-                />
-              </GridListTile>
-            ))}
+        <div className={classes.root} onScroll={e => props.handleScroll(e)}>
+          <GridList
+            id="image-collage"
+            spacing={1}
+            cellHeight="auto"
+            className="gridList"
+          >
+            {tileData.map(
+              (tile, index) =>
+                (tile.category === props.section ||
+                  props.section === ArchiveCategory.all) && (
+                  <GridListTile
+                    key={tile.img}
+                    cols={index === 0 ? 2 : 1}
+                    style={{ padding: "0.25rem" }}
+                  >
+                    <Image
+                      src={tile.img}
+                      disableError
+                      onClick={() => props.handleImageClick(tile)}
+                      style={{
+                        backgroundColor: "transparent",
+                        paddingTop:
+                          index === 0 && props.section === ArchiveCategory.all
+                            ? "40%"
+                            : tile.orientation === "horizontal"
+                            ? "75%"
+                            : "100%"
+                      }}
+                      imageStyle={{
+                        height: ""
+                      }}
+                    />
+                    <GridListTileBar
+                      title={tile.title}
+                      titlePosition="top"
+                      actionPosition="left"
+                      className={classes.titleBar}
+                      style={{ padding: mobile ? "0" : "0.5rem" }}
+                    />
+                  </GridListTile>
+                )
+            )}
           </GridList>
         </div>
       )}
