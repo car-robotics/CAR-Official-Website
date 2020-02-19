41ae20b2d5dec121424cf5d7346484d22eeec22a
Matt Wells <mwells21@uncc.edu>
2020-02-19 15:36:42 -0500

    Bug fixes and improvements (#9)
    * Sidebar is fixed, made officer cards taller, and entire div is no longer selectable for rules
    
    * More space for archive pictures
    
    * Small improvements
    
    * Small fixes
    
    * Wording change
    
    * update ignore
    
    * Small tweaks
    
    * Better Lightbox sizing, Archive menu redesign
    
    * Meta tags added, photo file refactor, archive broken needs refactor, rules added, Nathaniel added, small bug fixes
    
    * Fixed images
    
    * Increased mobile threshold, fixed img links
    
    * Logo resize and limit outreach images to 3
    
    * Mobile popout position fix
    
    * Maybe height is better to go off of?
    
    * Complicating the logo size!
    
    * Fix about page container sizes
    
    * Small bug fix and other content sizing
    
    * Timer spacing changes and tooltips added to outreach images
    
    * Fixed console errors
    
    * Better lightbox animation!
    
    * Small changes
    
    * Fixed Archive scrolling and other improvements
    
    * Nav style changes and the release of react-socks
    
    * Added images to Archive
    
    * Animate slideshow indicators
    
    * Mobile context dynamically updates
    
    * Styled feedback popover
    
    * Fixed school green, new background, put COLORS.ts in Theme
    
    * Update manifest and remove meta tags, update rules link for mobile
    
    * Small tweaks
    
    * Added Crown to navbar
    
    * Slideshow indicators now clickable
    
    * Added UNCC Makerspace to Sponsors
    
    * Made Makerspace logo look better
    
    * Fixed feedback popover on mobile
    
    * Changed cursor on slideshow indicators
    
    * Disable Image error
    
    * Added image, new hover transitions, goBilda added
    
    * Changed divs to appropriate tags
    
    * Fix lightbox size
    
    * Undo last changes
    
    * Fixed font family
    
    * Import Roboto as backup
    
    * Made max width for all containers
    
    * Added icon to make Archive menu more noticable
    
    * Update archive filter button
    
    * Font redesign (#7)
    
    * Fonts changed
    
    * Fonts manually dynamically done!
    
    * Small font changes
    
    * Remove officer card and outreach image hover transition
    
    * Better image grid layout
    
    * Small font changes
    
    * Fixed outreach container sizing
    
    * Reduce name in app bar on mobile
    
    * Archive image grid height increase, open rules in new window button added, footer text size decreased
    
    * Images have been made local!
    
    * Update version and change lightbox supporting text
    
    * Better sizing for zoom
    
    * Font fixes, slideshow logo picture fix, Better UNCC logo positioning
    
    * Fix IEEE logo
    
    * Images added
    
    * Small fixes
    
    * Added video, fixed IEEE logo
    
    * Revert last change

--- src/Sponsors/SponsorCard.tsx
+++ src/Sponsors/SponsorCard.tsx
@@ -6,68 +6,72 @@ import { COLORS } from "../Themes/COLORS";
 import SponsorCardCover from "./SponsorCardCover";
 
 interface SponsorsCardProps {
-    orgName: string;
-    orgLink?: string;
-    img: string;
-    square?: boolean;
+  orgName: string;
+  orgLink?: string;
+  img: string;
+  square?: boolean;
 }
 
 export default function SponsorCard(props: SponsorsCardProps) {
-    const [showCover, setShowCover] = React.useState<boolean>(false);
+  const [showCover, setShowCover] = React.useState<boolean>(false);
 
-    const { orgName, orgLink, img, square } = props;
-    return (
-        <>
-            <Card
-                onMouseEnter={() => setShowCover(true)}
-                onMouseLeave={() => setShowCover(false)}
-                className="orgCard"
-            >
-                {orgLink && <Tooltip title={`${orgName} - ${orgLink}`}>
-                    <Link href={orgLink} target="_blank">
-                        <div className="card-cover-container">
-                            <SponsorCardCover show={showCover} text={"Learn More"} />
-                            <Image
-                                src={img}
-                                imageStyle={{
-                                    display: "block",
-                                    position: "static",
-                                    transition: ".5s ease",
-                                    backfaceVisibility: "hidden",
-                                }}
-                                style={{
-                                    border: `12px solid ${COLORS.darkColor}`,
-                                    borderRadius: "1rem",
-                                    width: "14.5rem",
-                                    height: square ? "14.5rem" : "16.5rem",
-                                    padding: square ? "1rem 0" : "",
-                                    paddingTop: 0,
-                                }}
-                            />
-                        </div>
-                    </Link>
-                </Tooltip>}
+  const { orgName, orgLink, img, square } = props;
+  return (
+    <>
+      <Card
+        onMouseEnter={() => setShowCover(true)}
+        onMouseLeave={() => setShowCover(false)}
+        className="orgCard"
+      >
+        {orgLink && (
+          <Tooltip title={`${orgName} - ${orgLink}`}>
+            <Link href={orgLink} target="_blank">
+              <div className="card-cover-container">
+                <SponsorCardCover show={showCover} text={"Learn More"} />
+                <Image
+                  src={img}
+                  imageStyle={{
+                    display: "block",
+                    position: "static",
+                    transition: ".5s ease",
+                    backfaceVisibility: "hidden"
+                  }}
+                  style={{
+                    border: `12px solid ${COLORS.darkColor}`,
+                    borderRadius: "1rem",
+                    width: "14.5rem",
+                    height: square ? "14.5rem" : "16.5rem",
+                    padding: square ? "1rem 0" : "",
+                    paddingTop: 0
+                  }}
+                />
+              </div>
+            </Link>
+          </Tooltip>
+        )}
 
-                {!orgLink && <div className="card-cover-container">
-                    <SponsorCardCover show={showCover} text={orgName} />
-                    <Image
-                        src={img}
-                        imageStyle={{
-                            display: "block",
-                            position: "static",
-                            transition: ".5s ease",
-                            backfaceVisibility: "hidden",
-                        }}
-                        style={{
-                            border: `12px solid ${COLORS.darkColor}`,
-                            borderRadius: "1rem",
-                            width: "14.5rem",
-                            height: "16.5rem",
-                            paddingTop: 0,
-                        }}
-                    />
-                </div>}
-            </Card>
-        </>
-    );
-}    
+        {!orgLink && (
+          <div className="card-cover-container">
+            <SponsorCardCover show={showCover} text={orgName} />
+            <Image
+              src={img}
+              imageStyle={{
+                display: "block",
+                position: "static",
+                transition: ".5s ease",
+                backfaceVisibility: "hidden"
+              }}
+              style={{
+                border: `12px solid ${COLORS.darkColor}`,
+                borderRadius: "1rem",
+                width: "14.5rem",
+                height: "16.5rem",
+                paddingTop: 0
+              }}
+            />
+          </div>
+        )}
+      </Card>
+    </>
+  );
+}
